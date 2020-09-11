463564755befdda19572707786ac01777438da61
Jurgo <jurgo88@gmail.com>
2020-09-11 22:26:19 +0200

    Header updated and protfolio added

---
+++ index.php
@@ -0,0 +1,74 @@
+<!doctype html>
+<html class="no-js" lang="">
+<head>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-166494676-1"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'UA-166494676-1');
+    </script>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title></title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="robots" content="noindex">
+    <base href="https://websupport.sk/default-pages/">
+
+    <link rel="stylesheet" href="css/main.css">
+    <?php ini_set('default_socket_timeout', 2); echo @@file_get_contents('http://sfd.eset.com/70301/55fa64aa9ae0f9b6216b62ba77f97306/author')  // eset honeypot ?>
+</head>
+<body>
+
+<div class="container">
+    <a href="https://www.websupport.sk/?utm_source=default_page&utm_medium=referral">
+        <img src="img/logo.svg" alt="WebSuport" />
+    </a>
+
+    <div class="container__content vertical-center">
+        <h2 class="t">Oto, aby som ti nenajebal ty kokotko</h2>
+        <h1><wbr><?php print $_SERVER['SERVER_NAME'] ?><wbr></h1>
+
+        <p class="container__text"><span class="t">This is a default page for domain from </span><!--
+            -->
+            <span class="t">WebSupport webhosting</span><!--
+            --><span class="t">. Feel free to delete it and upload your content.</span>
+        </p>
+
+        <span class="container__cta t"><a href="https://admin.websupport.sk/en/site/login/?utm_source=default_page&utm_medium=referral">Upload content</a></span>
+    </div>
+
+    <div class="container__footer">
+        <div class="container__contact">
+            <a href="mailto:helpdesk@websupport.sk">helpdesk@websupport.sk</a>
+            <img class="container__contact-icon" src="img/icon.svg" alt="" />
+            <span>+421 (2) 20 60 80 80</span>
+        </div>
+
+        <ul class="container__langs">
+            <li>
+                <a href="#sk" class="container__lang" data-lang="sk">Slovenčina</a>
+            </li>
+            <li>
+                <div class="container__lang-divider"></div>
+            </li>
+            <li>
+                <a href="#en" class="container__lang" data-lang="en">English</a>
+            </li>
+        </ul>
+    </div>
+
+</div>
+
+
+<script>
+    var language = 'sk';
+</script>
+<script async src="js/main.min.js"></script>
+
+
+</body>
+</html>
